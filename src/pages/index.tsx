acee8ead946f12e07eb13a46105648ac3f7c0218
Lucas <lfontinele07@gmail.com>
2022-01-23 16:59:02 -0300

    Some changes

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+import styles from '../styles/Home.module.scss';
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <p>Hello World</p>
+    </div>
+  )
+}
+
+export default Home
